61cb563bbc0894f3a1c83be7967351abfe35b0cb
Valdinei Ferreira <valdineifer@outlook.com>
2018-09-16 02:57:30 -0300

    base da API feita, listagem geral concluida

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
+Route::group(array('prefix' => 'api'), function()
+{
+
+  Route::get('/', function () {
+      return response()->json(['message' => 'Jobs API', 'status' => 'Connected']);;
+  });
+
+  Route::resource('jobs', 'JobsController');
+  Route::resource('companies', 'CompaniesController');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('api');
 });
