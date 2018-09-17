afdcfe6c8b7c92d9f608ad669219e757969b47b0
Valdinei Ferreira <valdineifer@outlook.com>
2018-09-16 23:01:21 -0300

    update rotas to api.php

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::group(array('prefix' => 'api'), function()
-{
-
-  Route::get('/', function () {
-      return response()->json(['message' => 'Jobs API', 'status' => 'Connected']);;
-  });
-
-  Route::resource('jobs', 'JobsController');
-  Route::resource('companies', 'CompaniesController');
-});
-
-Route::get('/', function () {
-    return redirect('api');
-});
